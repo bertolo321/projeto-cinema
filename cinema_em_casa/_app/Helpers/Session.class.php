f6336eed42490610e94b010ac11c790350ed1e9e
André Bertolo <andre_bertolo@outlook.com>
2020-11-02 23:47:00 -0300

    Update Session.class.php

--- cinema_em_casa/_app/Helpers/Session.class.php
+++ cinema_em_casa/_app/Helpers/Session.class.php
@@ -4,7 +4,7 @@
  * Session.class [ HELPER ]
  * Responsável pelas estatísticas, sessões e atualizações de tráfego do sistema!
  * 
- * @copyright (c) 2014, Robson V. Leite UPINSIDE TECNOLOGIA
+ * @copyright (c) 2014, André Bertolo
  */
 class Session {
 
